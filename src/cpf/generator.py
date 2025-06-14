f222ccda1d732d370203e7e814f30746076fbf37
loboprogrammingg <matheusloboo2001@gmail.com>
2025-06-14 16:08:57 -0400

    Criando o validador de CPF

--- src/cpf/generator.py
+++ src/cpf/generator.py
@@ -1,6 +1,5 @@
 import random
 
-
 def generate_cpf(formatted: bool = True) -> str:
     def calculate_digit(digits):
         total = sum(
@@ -8,12 +7,10 @@ def generate_cpf(formatted: bool = True) -> str:
         )
         remainder = 11 - (total % 11)
         return '0' if remainder > 9 else str(remainder)
-    
     base = ''.join(str(random.randint(0,9)) for _ in range(9))
     d1 = calculate_digit(base)
     d2 = calculate_digit(base + d1)
     cpf = base + d1 + d2
-    
     if formatted:
         return f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
     return cpf
